56165f7aad13c4fc1bf943991386360c712c8896
Perry Raskin <perryraskin@gmail.com>
2020-10-17 23:05:27 -0400

    Setup Prisma and first migration

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,6 +1,8 @@
 import React from "react"
 import { NextPage } from "next"
 import Router from "next/router"
+import { useQuery, useMutation, queryCache } from "react-query"
+
 import withLayout from "../../hocs/withLayout"
 import utilities from "../../utilities"
 
@@ -8,7 +10,15 @@ import Button from "../Elements/Button"
 
 interface Props {}
 
+async function fetchUsersRequest() {
+  const res = await fetch("/api/users")
+  const data = await res.json()
+  const { users } = data
+  return users
+}
+
 const Home: NextPage<Props> = ({}) => {
+  const { data: users } = useQuery("users", fetchUsersRequest)
   return (
     <div className="text-center">
       <h1>Next.js TailwindCSS Starter</h1>
@@ -22,6 +32,10 @@ const Home: NextPage<Props> = ({}) => {
           extend="bg-blue-600 hover:bg-blue-500 text-white"
         />
       </a>
+      <p>
+        {users ? users[0].name : ""}'s email is {users ? users[0].email : ""}
+      </p>
+      <p>{JSON.stringify(users)}</p>
     </div>
   )
 }
